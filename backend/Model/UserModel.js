6df94c44b1d18214dccfdc34775d747df1d91f65
girirajPanchariya <gafsa3768@gmail.com>
2026-03-22 18:08:38 +0530

    message

--- backend/Model/UserModel.js
+++ backend/Model/UserModel.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -35,56 +34,3 @@ const userSchema = new mongoose.Schema({
 
 
 export const User = mongoose.model('User',userSchema)
-=======
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-    phone:{
-        type:String,
-        required:true,
-    },
-    SalingBook:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Prodect"
-    },
-  profile:[{
-    image:{
-        type:String,
-
-    },
-    address:[{
-        
-    city:{
-        type:String,
-    },
-    state:{
-        type:String,
-    },  
-    country:{
-        type:String,    
-    },
-    vilaage:{
-        type:String,
-    },
-    pincode:{
-        type:String,        
-    }
-
-}
-]
-  }]
-},{  timestamps: true,});
-
-export const UserModel = mongoose.model('User',userSchema);
->>>>>>> 8b5693da49838ff7bd6a6e720bed888783bef530
