52f22200733d2e00d89f1265dc49c6264f6107f5
girirajPanchariya <gafsa3768@gmail.com>
2026-03-22 17:54:34 +0530

    message

--- backend/Model/Prodect.js
+++ backend/Model/Prodect.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
 const ProdectSchema = mongoose.Schema({
@@ -30,41 +29,3 @@ const ProdectSchema = mongoose.Schema({
 },{timetimestamps:true})
 
 export const Prodect = mongoose.model('Prodect',ProdectSchema)
-=======
-import mongoose from "mongoose";    
-
-const Prodect = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    name:{  
-        type:String,
-        required:true,
-    },
-    price:{
-        type:Number,
-        required:true,  
-    },
-    image:{
-        type:String,
-        required:true,  
-    },
-    details:{
-        type:String,
-        required:true,  
-    },
-    // precherser:{
-    //     type:mongoose.Schema.Types.ObjectId,
-    //     ref:"User"  
-    // },
-    order:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Order'  
-    }    
-
-
-})
-
-export const ProdectModel = mongoose.model('Prodect',bookSchema);
->>>>>>> 8b5693da49838ff7bd6a6e720bed888783bef530
